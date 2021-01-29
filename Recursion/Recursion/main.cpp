58e6fa4650913092f090755bb6706524bf7620db
vovaZuev <vovazuev@yandex.ru>
2021-01-29 07:31:50 +0700

    All is DONE

--- Recursion/Recursion/main.cpp
+++ Recursion/Recursion/main.cpp
@@ -8,22 +8,24 @@ double Power(double n, int p);
 
 int fib_num(int n);
 
-void Fibon(int n, double a = 0, double b = 1);
+void FibNum(int n, double a = 0, double b = 1);
+
+void FibMaxValue(int max, double a = 0, double b = 1);
 
 int main()
 {
 	setlocale(LC_ALL, "");
 	double n;
 	int p;
-	int num;
+	int lim, num;
 	cout << "¬ведите число и степень: "; cin >> n >> p;
 	cout << Power(n, p) << endl;
 	cout << delimiter;
-	/*cout << "¬ведите предел дл€ чисел ‘ибоначчи: "; cin >> lim;
-	Fib1(lim);
-	cout << delimiter;*/
+	cout << "¬ведите предел дл€ чисел ‘ибоначчи: "; cin >> lim;
+	FibMaxValue(lim);
+	cout << delimiter;
 	cout << "¬ведите пор€дковый номер числа ‘ибоначчи: "; cin >> num;
-	Fibon(num);
+	FibNum(num);
 }
 double Power(double n, int p)
 {
@@ -40,9 +42,16 @@ int fib_num(int n)
 }
 
 
-void Fibon(int n, double a, double b)
+void FibNum(int n, double a, double b)
 {
 	if (n == 0) return;
 	cout << a << "\n";
-	Fibon(n - 1, b, a + b);
+	FibNum(n - 1, b, a + b);
+}
+
+void FibMaxValue(int max, double a, double b)
+{
+	if (a > max) return;
+	cout << a << "\n";
+	FibMaxValue(max, b, a + b);
 }
